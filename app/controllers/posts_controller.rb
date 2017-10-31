848a260309079481937ac34f06ac2ca13552a047
Micah Redding <micahtredding@gmail.com>
2017-10-31 00:09:35 -0500

    Newest posts first

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
     @post = Post.new
     render layout: false
   end
