8d67e3c7b3125d499590564c65703a1fc06d2066
Micah Redding <micahtredding@gmail.com>
2017-10-30 23:51:36 -0500

    Put form on posts#index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    @post = Post.new
     render layout: false
   end
 
@@ -25,10 +26,10 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
-        format.html { render :new }
+        format.html { rendirect_to posts_path }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
